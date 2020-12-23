ab5ad5bc8fc0aa458ee669a502899a80dfed8222
Khumairi Zulhizmarch <khumairiari21@gmail.com>
2020-12-23 09:15:04 +0700

    Add project files.

---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string Insert(InsertUser user)
+        {
+            string msg;
+            SqlConnection con = new SqlConnection("Data Source=ARILAND;Initial Catalog=Test;User ID=sa;Password=020320Ari;Pooling=False");
+            con.Open();
+                SqlCommand cmd = new SqlCommand("Insert into UserTab (Name, Email) values(@Name, @Email)", con);
+            cmd.Parameters.AddWithValue("@Name", user.Name);
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+
+            int g = cmd.ExecuteNonQuery();
+            if(g==1)
+            {
+                msg = "Successfully Inserted";
+            }
+            else
+            {
+                msg = "Failed to insert";
+            }
+            return msg;
+        }
+
+        
+    }
+}
